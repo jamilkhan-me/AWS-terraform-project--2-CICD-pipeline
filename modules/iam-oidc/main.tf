fb3250a9657b31601f130af4675ee50d67468e6c
Jamil Khan <jamilkhan.nu@gmail.com>
2026-08-17 20:28:08 +0100

    oidc modified

--- modules/iam-oidc/main.tf
+++ modules/iam-oidc/main.tf
@@ -20,14 +20,20 @@ resource "aws_iam_role" "github_actions" {
         Effect    = "Allow"
         Principal = { Federated = aws_iam_openid_connect_provider.github.arn }
         Action    = "sts:AssumeRoleWithWebIdentity"
+
         Condition = {
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-          }
-          StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/main"
-          }
-        }
+  StringEquals = {
+    "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+  }
+}
+        # Condition = {
+        #   StringEquals = {
+        #     "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+        #   }
+        #   StringLike = {
+        #     "token.actions.githubusercontent.com:sub" = "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/main"
+        #   }
+        # }
       }
     ]
   })
